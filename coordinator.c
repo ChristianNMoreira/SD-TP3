08390b12ed542a591a1604c92afafcce8289a65b
Christian Moreira <christian.moreira@poli.ufrj.br>
2023-07-17 22:57:40 -0300

    Add first idea of coordinator

--- coordinator.c
+++ coordinator.c
@@ -3,6 +3,8 @@
 #include <stdbool.h>
 #include <unistd.h>
 
+#include <pthread.h>
+
 #include <arpa/inet.h>
 #include <strings.h>
 #include <string.h>
@@ -17,6 +19,16 @@
 
 #define F 10
 
+struct cancelStruct {
+    pthread_t listen;
+    pthread_t coordinate;
+};
+
+pthread_mutex_t mutex_queue = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_file = PTHREAD_MUTEX_INITIALIZER;
+
+char requestQueue[100];
+
 void *terminalThread(void *args) {
     bool loop_terminal = 1;
     do {
@@ -27,14 +39,16 @@ void *terminalThread(void *args) {
         } else if (i == 2) {
             printf("quantas vezes cada processo foi atendido");
         } else if (i == 3) {
-            pthread_cancel(args);
+            struct cancelStruct *cS = args;
+            pthread_cancel(cS->coordinate);
+            pthread_cancel(cS->listen);
             loop_terminal = 0;
         }
     } while (loop_terminal == 1);
 }
 
-void *coordinatorThread(void *args) {
-    int sockfd, connfd, len;
+void *coordinatorListener(void *args) {
+    int sockfd;
 	struct sockaddr_in servaddr, cli;
 
 	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -45,7 +59,7 @@ void *coordinatorThread(void *args) {
 	bzero(&servaddr, sizeof(servaddr));
 
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	servaddr.sin_port = htons(PORT);
 
     if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
@@ -54,14 +68,13 @@ void *coordinatorThread(void *args) {
 	}
 
     char message[F];
-    char *msg, *prcs, *content
-    bool loop = 1;
+    char *msg, *prcs, *content;
+    bool loop_listen = 1;
     int num;
 
     do {
         bzero(message, sizeof(message));
-        if (recvfrom(sockfd, message, sizeof(message), 0,
-            (SA*)&cli, sizeof(cli))) {
+        if (recvfrom(sockfd, message, sizeof(message), 0, (SA*)&cli, sizeof(cli))) {
             printf("Erro ao receber mensagem\n");
             exit(0);
         }
@@ -71,10 +84,52 @@ void *coordinatorThread(void *args) {
         prcs = token;
         token = strtok(NULL, "|");
         content = token;
+        if (msg == "1") {
+            // REQUEST
+            pthread_mutex_lock(&mutex_queue);
+            // Adicionar mensagem à queue
+            // Salvar informações da struct cli e do inteiro sockfd para recuperar o processo cliente
+            pthread_mutex_unlock(&mutex_queue);
+        }
+        if (msg == "3") {
+            // RELEASE
+            pthread_mutex_unlock(&mutex_file);
+        }
         
-    } while (loop == 1);
+    } while (loop_listen == 1);
+}
+
+void *coordinatorManager(void *args) {
+    bool loop_coordinator = 1;
+
+    char message[F];
+
+    int sockfd;
+	struct sockaddr_in cli;
+    do {
+        pthread_mutex_lock(&mutex_queue);
+        // ler mensagens na queue e retirar da queue
+        pthread_mutex_unlock(&mutex_queue);
+        // buscar informação de cli e de sockfd
+        bzero(message, sizeof(message));
+        // envia mensagem ao cliente
+        // manipular message para que seja GRANT
+        if (sendto(sockfd, message, sizeof(message), 0, (SA*)&cli, sizeof(cli)) < 0){
+            printf("Can't send\n");
+            return -1;
+        }
+    } while (loop_coordinator == 1);
 }
 
 int main() {
-    
+    pthread_t terminal, listen, coordinate;
+
+    pthread_create(&listen, NULL, &coordinatorListener, NULL);
+    pthread_create(&coordinate, NULL, &coordinatorManager, NULL);
+
+    struct cancelStruct cS;
+    cS.listen = listen;
+    cS.coordinate = coordinate;
+
+    pthread_create(&terminal, NULL, &terminalThread, (void *)&cS);
 }
