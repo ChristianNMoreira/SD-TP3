df937042ed90a79436a970e3c59be2f029a9d361
Christian Moreira <christian.moreira@poli.ufrj.br>
2023-07-18 23:59:02 -0300

    Final version

--- coordinator.py
+++ coordinator.py
@@ -25,10 +25,6 @@ def terminalThread():
     while True:
         in_terminal = input("Input terminal: ")
         if in_terminal == "1":
-            # with queue_lock:
-            #     print("Fila de pedidos atual:")
-            #     for item in queue:
-            #         print(item)
 
             queue_lock.acquire()
             print("Fila de pedidos atual:")
@@ -37,10 +33,6 @@ def terminalThread():
             queue_lock.release()
 
         elif in_terminal == "2":
-            # with processes_lock:
-            #     print("Quantas vezes cada processo foi atendido:")
-            #     for processo, contador in processes.items():
-            #         print(f"Processo {processo}: {contador} vezes")
             processes_lock.acquire()
             print("Quantas vezes cada processo foi atendido:")
             for processo, contador in processes.items():
@@ -54,33 +46,24 @@ def coordinatorListener():
         rcv = UDPServerSocket.recvfrom(buffer_size)
         message = rcv[0].decode()
         address = rcv[1]
-        message = message.split("|")
-        msg = message[0]
-        prcss = message[1]
-        content = message[2]
+        msg, prcss, content = message.split("|")
         if msg == REQUEST:
             queue_lock.acquire()
             queue.append((msg, prcss, content, address))
             queue_lock.release()
-            # log_lock.acquire()
-            # with open('log.txt', 'a') as f:
-            #     f.write(f"[R] Request | {prcss} | {datetime.now().strftime('%H:%M:%S.%f')}\n")
-            # log_lock.release()
-        if msg == RELEASE:
-            # log_lock.acquire()
-            # with open('log.txt', 'a') as f:
-            #     f.write(f"[R] Release | {prcss} | {datetime.now().strftime('%H:%M:%S.%f')}\n")
-            # log_lock.release()
+            log_lock.acquire()
+            with open('log.txt', 'a') as f:
+                f.write(f"[R] REQUEST | {prcss} | {datetime.now().strftime('%H:%M:%S.%f')}\n")
+            log_lock.release()
+        elif msg == RELEASE:
             file_lock.release()
-
+            log_lock.acquire()
+            with open('log.txt', 'a') as f:
+                f.write(f"[R] RELEASE | {prcss} | {datetime.now().strftime('%H:%M:%S.%f')}\n")
+            log_lock.release()
 
 def coordinatorManager():
     while True:
-        # with queue_lock:
-        #     if queue:
-        #         msg, prcss, content, address = queue.pop(0)
-        #     else:
-        #         continue
         queue_lock.acquire()
         if queue:
             msg, prcss, content, address = queue.pop(0)
@@ -88,14 +71,7 @@ def coordinatorManager():
             queue_lock.release()
             continue
         queue_lock.release()
-
-        file_lock.acquire()
         
-        # with processes_lock:
-        #     if prcss not in processes:
-        #         processes[prcss] = 1
-        #     else:
-        #         processes[prcss] += 1
         processes_lock.acquire()
         if prcss not in processes:
             processes[prcss] = 1
@@ -103,11 +79,12 @@ def coordinatorManager():
             processes[prcss] += 1
         processes_lock.release()
 
-        # log_lock.acquire()
-        # with open('log.txt', 'a') as f:
-        #     f.write(f"[S] GRANT | {prcss} | {datetime.now().strftime('%H:%M:%S.%f')}\n")
-        # log_lock.release()
+        log_lock.acquire()
+        with open('log.txt', 'a') as f:
+            f.write(f"[S] GRANT | {prcss} | {datetime.now().strftime('%H:%M:%S.%f')}\n")
+        log_lock.release()
 
+        file_lock.acquire()
         UDPServerSocket.sendto(f"{GRANT}|{prcss}|{content}".encode(), address)
 
 
